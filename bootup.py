2cf7f3bd962cfc8b3c099481a60c76121963713f
anshupandey <ansh@abcxxx.com>
2020-06-02 02:42:04 +0530

    linear regression mathematical code

---
+++ bootup.py
@@ -0,0 +1,15 @@
+class employee:
+    Name = "John Robinson"
+    Age = 20
+    Biography = """
+    lorem epsun lorem ...........................
+    ............
+    """
+    Salary = 0
+    Status = True
+    
+    def resignation(self):
+        self.Status=False
+        print("you resigned, check your status")
+        
+    
